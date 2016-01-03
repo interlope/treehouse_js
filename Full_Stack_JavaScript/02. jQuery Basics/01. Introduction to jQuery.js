25adfba83e9365e2e8f3692ada7a1c4fdabc3690
Gino Valente <gino.valente3@gmail.com>
2016-01-03 09:31:07 -0800

    Added new files for jQuery basics and redid file structure

---
+++ Full_Stack_JavaScript/02. jQuery Basics/01. Introduction to jQuery.js
@@ -0,0 +1,68 @@
+01. Introduction to jQuery
+=======================================
+
+---------------------------------------
+
+// Ubobtrusive JavaScript
+// JS that doesn't get in the way of the user or cares of their environment
+
+// jQuery documentation
+// http://api.jquery.com/
+
+// Calling jQuery
+jQuery();
+$();
+
+// What is the DOM
+//Docucment Object Model
+//Traversing the DOM - Moving through the DOM 
+
+// DOM MANIPULATION
+//Add/Remove Elements
+//Update/Read Element Attributes
+//Transform HTML Elements
+
+// TRAVERSING THE DOM
+//Move from parent to children / other way
+//Sibling elements
+
+// EVENT METHODS
+//Keyboard or mouse events
+
+
+
+// Document Ready
+$(document).ready(function() {
+	//do stuff
+});
+//or shorthand
+$(function() {
+	//do stuff
+});
+
+//use ready when calling the JS in the head
+//if JS is at end of body then DOM is already loaded
+
+//*Load JS at Bottom. Speeds up, slightly faster, loads images before JS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
